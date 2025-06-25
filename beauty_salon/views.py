f47cc5d04147fd99389b2f98e98e34d1687ad1ca
yelena0000 <kirillnek07081999@gmail.com>
2025-06-25 20:12:53 +0300

    Add static files, configure URLs and use Django templates

--- beauty_salon/views.py
+++ beauty_salon/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def service_finally(request):
+    return render(request, 'serviceFinally.html')
+
+
+def notes(request):
+    return render(request, 'notes.html')
+
+
+def popup(request):
+    return render(request, 'popup.html')

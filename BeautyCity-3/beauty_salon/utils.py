1486f04bda231c38b209026be172b941ce9b39c2
yelena0000 <kirillnek07081999@gmail.com>
2025-06-28 03:45:57 +0300

    Delete accidental repository

--- BeautyCity-3/beauty_salon/utils.py
+++
@@ -1,9 +0,0 @@
-from phonenumbers import NumberParseException, is_valid_number, parse
-
-
-def validate_phone(phone):
-    try:
-        phone_obj = parse(phone, "RU")
-        return is_valid_number(phone_obj)
-    except NumberParseException:
-        return False
